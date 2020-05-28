359108ed9aea73479e882a42e2b0d44d17c7fa5c
Piotr Pacewicz <piotrp92@gmail.com>
2020-05-28 18:01:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal(".my-nav-button", {
+  interval: 350
+});
+
+ScrollReveal().reveal(".my-contact-cell", {
+  interval: 300
+});
+
+ScrollReveal().reveal(".my-jumbotron-slogan", {
+  interval: 300
+});
